fd6225e7358122b1f954f6109fe0ddaa286d2dc8
Bas Vodde <basv@odd-e.com>
2017-03-08 17:30:14 +0530

    Added a context and renamed one more result of wine

--- spec/svn_log_parser_spec.rb
+++ spec/svn_log_parser_spec.rb
@@ -1,7 +1,9 @@
 
 describe "parsing the SVN logs from repositories" do
 
-  csv_log_xml = '<?xml version="1.0" encoding="UTF-8"?>
+  subject { SVNLogParser.new }
+  context "SVN Log with 2 commits and 3 modified files" do
+    svn_log_xml = '<?xml version="1.0" encoding="UTF-8"?>
 <log>
 <logentry
    revision="10">
@@ -36,45 +38,44 @@ describe "parsing the SVN logs from repositories" do
 </logentry>
 </log>'
 
-  subject { SVNLogParser.new }
+    before(:each) do
+      @participants = Participants.new(["basvodde", "daniel"])
+      subject.xml_to_parse(svn_log_xml)
+      subject.participants = @participants
+      subject.repository = Repository.new("repo")
+    end
 
-  before(:each) do
-    @participants = Participants.new(["basvodde", "daniel"])
-    subject.xml_to_parse(csv_log_xml)
-    subject.participants = @participants
-    subject.repository = Repository.new("repo")
-  end
+    it "parses SVN log files" do
+      subject.participants = Participants.new("basvodde")
+      subject.parse
+      commits = subject.commits
+      expect(commits.amount).to eq 1
+    end
 
-  it "parses SVN log files" do
-    subject.participants = Participants.new("basvodde")
-    subject.parse
-    commits = subject.commits
-    expect(commits.amount).to eq 1
-  end
+    it "knows Bas had one commit" do
+      subject.parse
+      expect(@participants.find_by_loginname("basvodde").commits.amount).to eq 1
+    end
 
-  it "knows Bas had one commit" do
-    subject.parse
-    expect(@participants.find_by_loginname("basvodde").commits.amount).to eq 1
-  end
+    it "knows all the details of the commit" do
+      subject.parse
+      commit = subject.commits.find_commit("10")
+      expect(commit.message).to eq "Summary:optimize implementation."
+      expect(commit.author).to eq "basvodde"
+      expect(commit.participant.loginname).to eq "basvodde"
+      expect(commit.date).to eq DateTime.new(2016,1,4,1,59,13)
+    end
 
-  it "knows all the details of the commit" do
-    subject.parse
-    commit = subject.commits.find_commit("10")
-    expect(commit.message).to eq "Summary:optimize implementation."
-    expect(commit.author).to eq "basvodde"
-    expect(commit.participant.loginname).to eq "basvodde"
-    expect(commit.date).to eq DateTime.new(2016,1,4,1,59,13)
-  end
+    it "adds the repository information to each commit" do
+      commit = subject.parse.commits.find_commit("10")
+      expect(commit.repository.name).to eq "repo"
+    end
 
-  it "adds the repository information to each commit" do
-    commit = subject.parse.commits.find_commit("10")
-    expect(commit.repository.name).to eq "repo"
-  end
-
-  it "knows which files were changed in the commit" do
-    commit = subject.parse.commits.find_commit("10")
-    expect(commit.changes.size).to eq 1
-    expect(commit.changes[0].type).to eq :modified
-    expect(commit.changes[0].file).to eq "/trunk/header.hpp"
+    it "knows which files were changed in the commit" do
+      commit = subject.parse.commits.find_commit("10")
+      expect(commit.changes.size).to eq 1
+      expect(commit.changes[0].type).to eq :modified
+      expect(commit.changes[0].file).to eq "/trunk/header.hpp"
+    end
   end
 end
