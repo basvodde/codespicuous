fe685abb510f6890a9c375763d888e29fe1bf8c3
Bas Vodde <basv@odd-e.com>
2017-03-13 12:12:24 -0700

    Added additional configuration options

--- lib/codespicuous/codespicuousconfigurator.rb
+++ lib/codespicuous/codespicuousconfigurator.rb
@@ -1,6 +1,14 @@
-
+require 'yaml'
 
 class CodespicuousConfigurator
+
+  def config_options
+    options = YAML::load(File.read("codespicuous.yaml"))
+    puts '** Configuring options with "codespicuous.yaml"'
+    options
+  rescue Errno::ENOENT
+    {}
+  end
 
   def config_repositories
     puts '** Configuring repositories with "repositories.csv"'
--- spec/codespicuousconfigurator_spec.rb
+++ spec/codespicuousconfigurator_spec.rb
@@ -3,6 +3,17 @@ describe "CodepicuousConfigurator reads all the config files and provides the da
 
   subject { CodespicuousConfigurator.new }
 
+  it "reads the codespicuous configuration from YAML file when the file exists" do
+    expect(File).to receive(:read).with("codespicuous.yaml").and_return("option: setting")
+    expect(subject).to receive(:puts).with('** Configuring options with "codespicuous.yaml"')
+    expected = { "option" => "setting" }
+    expect(subject.config_options).to eq expected
+  end
+
+  it "Ignores the missing options file and tries to use defaults" do
+    expect(subject.config_options).to be_empty
+  end
+
   it "reads the repositories from file by default" do
     expect(File).to receive(:read).with("repositories.csv").and_return("name,url\nrepos,https://repos.com")
     expect(subject).to receive(:puts).with('** Configuring repositories with "repositories.csv"')
